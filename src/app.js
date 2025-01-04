b125adc653b3ebb17052353d761d4ca0116e5a2c
Ankur77720 <prajapatiankur8349@gmail.com>
2025-01-04 19:42:24 +0530

    Add user registration route and integrate cookie-parser middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
+import userRoutes from './routes/user.routes.js';
+import cookieParser from 'cookie-parser';
+
+
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+app.use('/users', userRoutes);
 
 export default app;
