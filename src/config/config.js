5583bab504e3a067650baff01edf4130ff49db1f
Ankur77720 <prajapatiankur8349@gmail.com>
2025-01-04 18:51:31 +0530

    Add user model with JWT authentication and password hashing

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,8 @@ dotEnv.config();
 const _config = {
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
 }
 
 const config = Object.freeze(_config);
