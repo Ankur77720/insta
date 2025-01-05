99fb27a4b238b080f0e2cd6de49b989e7189196c
Ankur77720 <prajapatiankur8349@gmail.com>
2025-01-05 14:41:26 +0530

    Implement user creation logic with error handling and token generation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import userModel from "../models/user.js";
 import { validationResult } from "express-validator";
+import * as userService from "../services/user.service.js";
 
 
 
@@ -12,9 +13,18 @@ export const createUserController = async (req, res) => {
         return res.status(400).json({ errors: errors.array() });
     }
 
+    try {
 
+        const { username, email, password } = req.body;
 
-    console.log(req.body)
+        const user = await userService.createUser({ username, email, password });
+        const token = user.generateToken();
 
-    res.send('register user');
+        res.status(201).json({ user, token });
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).send(error.message);
+    }
 }
