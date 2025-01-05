5ce44e18f70cd7f39f75af503f6396c746f724e2
Ankur77720 <prajapatiankur8349@gmail.com>
2025-01-05 14:18:39 +0530

    Add user registration validation middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,28 +1,13 @@
 import { Router } from "express";
 import * as userController from "../controllers/user.controller.js"
-import { body } from "express-validator";
+import * as userMiddleware from "../middlewares/user.middleware.js"
+
 const router = Router();
 
 
 
 router.post('/register',
-    body('username')
-        .isString()
-        .withMessage('Username must be a string')
-        .isLength({ min: 3, max: 15 })
-        .withMessage('Username must be between 3 and 15 characters')
-        .custom((value) => value === value.toLowerCase())
-        .withMessage('Username must be lowercase'),
-    body('email')
-        .isEmail()
-        .withMessage('Email must be a valid email'),
-    body('password')
-        .isString()
-        .withMessage('Password must be a string')
-        .isLength({ min: 6 })
-        .withMessage("Password must be at least 6 characters"),
-
-
+    userMiddleware.registerUserValidation,
     userController.createUserController)
 
 
