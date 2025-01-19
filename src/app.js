e37f6acab4550e289a0b12418f3c5560d5188f07
Ankur77720 <prajapatiankur8349@gmail.com>
2025-01-19 14:34:56 +0530

    Add image upload functionality and caption generation for posts

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import userRoutes from './routes/user.routes.js';
 import aiRoutes from './routes/ai.routes.js';
+import postsRoutes from "./routes/posts.routes.js"
 import cookieParser from 'cookie-parser';
 
 
@@ -13,5 +14,6 @@ app.use(cookieParser());
 
 app.use('/users', userRoutes);
 app.use('/ai', aiRoutes);
+app.use('/posts', postsRoutes);
 
 export default app;
