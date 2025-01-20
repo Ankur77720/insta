a5fdecec7dd65b7519cb788502b1e34b4e098755
Ankur77720 <prajapatiankur8349@gmail.com>
2025-01-20 11:50:17 +0530

    Add Cloudinary integration for image uploads in post creation

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,13 +1,16 @@
 import { generateCaptionFromImageBuffer } from "../services/ai.service.js"
+import { uploadFile } from "../services/cloudStorage.service.js";
+
 
 export const createPost = async (req, res, next) => {
 
     const imageBuffer = req.file.buffer;
 
-    const caption = await generateCaptionFromImageBuffer(imageBuffer);
+    // const caption = await generateCaptionFromImageBuffer(imageBuffer);
+    const fileData = await uploadFile(imageBuffer);
 
     res.status(201).json({
-        caption
+        fileData
     });
 
 
