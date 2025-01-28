ef197c39ad1dd8709d3f8a3a449c3ec5cfaa447b
Ankur77720 <prajapatiankur8349@gmail.com>
2025-01-28 13:20:32 +0530

    Add user authentication middleware for Socket.io connections

--- src/sockets/socket.io.js
+++ src/sockets/socket.io.js
@@ -1,4 +1,5 @@
 import { Server } from "socket.io";
+import userModel from "../models/user.js";
 
 
 function initSocket(server) {
@@ -7,7 +8,46 @@ function initSocket(server) {
 
     const io = new Server(server)
 
+    io.use(async (socket, next) => {
+
+        try {
+
+            const token = socket.handshake.headers.token;
+
+            if (!token) {
+                return next(new Error("Token is required"));
+            }
+
+            const decodedToken = userModel.verifyToken(token);
+
+            const user = await userModel.findById(decodedToken._id);
+
+            if (!user) {
+                return next(new Error("User not found"))
+            }
+
+            socket.user = user;
+
+            next();
+
+        } catch (err) {
+            next(err)
+        }
+
+
+    })
+
     io.on("connection", (socket) => {
+
+        socket.join(socket.user._id.toString())
+
+
+        socket.on("disconnect", () => {
+            console.log("User disconnected")
+            socket.leave(socket.user._id.toString())
+        })
+
+
         console.log("User connected")
     })
 
